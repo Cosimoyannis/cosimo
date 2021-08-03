7a9f0e5ee7be281f8a51e202d72d7ad2ffa341e6
Cosimoyannis <Cosimo.t@live.de>
2021-08-03 14:58:53 +0200

    start

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -3,7 +3,8 @@ package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-//import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication
 public class Application {
